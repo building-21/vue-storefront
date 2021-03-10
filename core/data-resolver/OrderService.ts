0d444cf4839ca27a611d8b0f8eac77dce1a8d998
JaviCandia <javi-candia@hotmail.com>
2021-03-10 04:40:58 +0000

    fixed unit test

--- core/data-resolver/OrderService.ts
+++ core/data-resolver/OrderService.ts
@@ -5,7 +5,7 @@ import { TaskQueue } from '@vue-storefront/core/lib/sync'
 import Task from '@vue-storefront/core/lib/sync/types/Task'
 import getApiEndpointUrl from '@vue-storefront/core/helpers/getApiEndpointUrl';
 
-const placeOrder = (order?: Order): Promise<Task> => {
+const placeOrder = (order?: Order): Promise<Task> =>
   TaskQueue.execute({
     url: 'http://localhost/vsfapi/javi/api/cart/create', // sync the order
     payload: {
@@ -15,7 +15,6 @@ const placeOrder = (order?: Order): Promise<Task> => {
       body: JSON.stringify(order)
     }
   })
-}
 export const OrderService: DataResolver.OrderService = {
   placeOrder
 }
